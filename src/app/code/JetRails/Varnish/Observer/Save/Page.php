a79173b2f3fd0f20e4c762994869324e62421633
Rafael Grigorian <me@raffi.io>
2018-08-16 15:40:11 -0500

    Fixed GH-11

--- src/app/code/JetRails/Varnish/Observer/Save/Page.php
+++ src/app/code/JetRails/Varnish/Observer/Save/Page.php
@@ -2,13 +2,8 @@
 
 	namespace JetRails\Varnish\Observer\Save;
 
-	use JetRails\Varnish\Helper\Data;
-	use JetRails\Varnish\Helper\Purger;
-	use JetRails\Varnish\Logger\Logger;
+	use JetRails\Varnish\Observer\AutoPurge;
 	use Magento\Framework\Event\Observer;
-	use Magento\Framework\Event\ObserverInterface;
-	use Magento\Framework\Message\ManagerInterface;
-	use Magento\Store\Model\StoreManagerInterface;
 
 	/**
 	 * Page.php - This observer is triggered when the CMS page save event is fired.  It then finds
@@ -20,46 +15,7 @@
 	 * @copyright       JetRails®, all rights reserved
 	 * @license         MIT https://opensource.org/licenses/MIT
 	 */
-	class Page implements ObserverInterface {
-
-		/**
-		 * These internal data members include instances of helper classes that are injected into
-		 * the class using dependency injection on runtime.
-		 * @var         Data                _data               Instance of the Data helper class
-		 * @var         Logger              _logger             Instance of the custom Logger class
-		 * @var         ManagerInterface    _messageManager     Instance of the ManagerInterface
-		 * @var         Purger              _purger             Instance of the Purger helper class
-		 * @var         StoreManager        _storeManager       Instance of the StoreManager
-		 */
-		protected $_data;
-		protected $_logger;
-		protected $_messageManager;
-		protected $_purger;
-		protected $_storeManager;
-
-		/**
-		 * This constructor is overloaded from the parent class in order to use dependency injection
-		 * to get the dependency classes that we need for this module's command actions to execute.
-		 * @param       Data                data                Instance of the Data helper class
-		 * @param       Logger              logger              Instance of the custom Logger class
-		 * @param       ManagerInterface    messageManager      Instance of the ManagerInterface
-		 * @param       Purger              purger              Instance of the Purger helper class
-		 * @param       StoreManager        storeManager        Instance of the StoreManager
-		 */
-		public function __construct (
-			Data $data,
-			Logger $logger,
-			ManagerInterface $messageManager,
-			Purger $purger,
-			StoreManagerInterface $storeManager
-		) {
-			// Save injected class instances
-			$this->_data = $data;
-			$this->_logger = $logger;
-			$this->_messageManager = $messageManager;
-			$this->_purger = $purger;
-			$this->_storeManager = $storeManager;
-		}
+	class Page extends AutoPurge {
 
 		/**
 		 * This method is required because this class implements the ObserverInterface class.  This
@@ -71,46 +27,9 @@
 		public function execute ( Observer $observer ) {
 			// Check to see if event is enabled
 			if ( $this->_data->isEnabled () && $this->_data->shouldPurgeAfterCmsPageSave () ) {
-				// Get the base url for the current store
-				$baseUrl = $this->_storeManager->getStore ()->getBaseUrl ();
-				// Get the product id and retrieve all rewrites recursively and acyclicly.
+				// Get id and purge all urls related to route
 				$pid = $observer->getPage ()->getId ();
-				$rewrites = $this->_purger->getUrlRewrites ("cms/page/view/page_id/$pid");
-				// Define purge command for each found URL
-				$actions = array_fill_keys ( $rewrites, "purgeUrl" );
-				$actions ["cms/page/view/page_id/$pid/"] = "purgeStore"; // note the trailing slash
-				// Loop through each rewrite
-				foreach ( $actions as $rewrite => $command ) {
-					// Validate the url
-					$url = $this->_purger->validateUrl ( $baseUrl . $rewrite );
-					// Loop though responses
-					foreach ( $this->_purger->{ $command } ( $url ) as $response ) {
-						// Log what we are trying to do
-						$message = [
-							"status" => $response->status,
-							"action" => "auto_purge:cms_save",
-							"target" => $response->target,
-							"server" => $response->server
-						];
-						$this->_logger->blame ( $this->_data->getLoggedInUserInfo (), $message );
-						// Check to see if response was successful
-						if ( $response->status == 200 ) {
-							// Add success response message
-							$targetHtml = "<font color='#79A22E' ><b>$response->target</b></font>";
-							$serverHtml = "<font color='#79A22E' ><b>$response->server</b></font>";
-							$message = "Successfully purged varnish cache for $targetHtml on $serverHtml";
-							$this->_messageManager->addSuccess ( $message );
-						}
-						else {
-							// Otherwise add an error message
-							$targetHtml = "<font color='#E22626' ><b>$response->target</b></font>";
-							$serverHtml = "<font color='#E22626' ><b>$response->server</b></font>";
-							$statusHtml = "<font color='#E22626' ><b>$response->status</b></font>";
-							$message = "Error Purging varnish cache for $targetHtml on $serverHtml with response code $statusHtml";
-							$this->_messageManager->addError ( $message );
-						}
-					}
-				}
+				$this->_purgeUsingRoute ("cms/page/view/page_id/$pid");
 			}
 		}
 
--- src/app/code/JetRails/Varnish/Observer/Save/Product.php
+++ src/app/code/JetRails/Varnish/Observer/Save/Product.php
@@ -2,13 +2,8 @@
 
 	namespace JetRails\Varnish\Observer\Save;
 
-	use JetRails\Varnish\Helper\Data;
-	use JetRails\Varnish\Helper\Purger;
-	use JetRails\Varnish\Logger\Logger;
+	use JetRails\Varnish\Observer\AutoPurge;
 	use Magento\Framework\Event\Observer;
-	use Magento\Framework\Event\ObserverInterface;
-	use Magento\Framework\Message\ManagerInterface;
-	use Magento\Store\Model\StoreManagerInterface;
 
 	/**
 	 * Product.php - This observer is triggered when the product save event is fired.  It then finds
@@ -20,47 +15,7 @@
 	 * @copyright       JetRails®, all rights reserved
 	 * @license         MIT https://opensource.org/licenses/MIT
 	 */
-	class Product implements ObserverInterface {
-
-
-		/**
-		 * These internal data members include instances of helper classes that are injected into
-		 * the class using dependency injection on runtime.
-		 * @var         Data                _data               Instance of the Data helper class
-		 * @var         Logger              _logger             Instance of the custom Logger class
-		 * @var         ManagerInterface    _messageManager     Instance of the ManagerInterface
-		 * @var         Purger              _purger             Instance of the Purger helper class
-		 * @var         StoreManager        _storeManager       Instance of the StoreManager
-		 */
-		protected $_data;
-		protected $_logger;
-		protected $_messageManager;
-		protected $_purger;
-		protected $_storeManager;
-
-		/**
-		 * This constructor is overloaded from the parent class in order to use dependency injection
-		 * to get the dependency classes that we need for this module's command actions to execute.
-		 * @param       Data                data                Instance of the Data helper class
-		 * @param       Logger              logger              Instance of the custom Logger class
-		 * @param       ManagerInterface    messageManager      Instance of the ManagerInterface
-		 * @param       Purger              purger              Instance of the Purger helper class
-		 * @param       StoreManager        storeManager        Instance of the StoreManager
-		 */
-		public function __construct (
-			Data $data,
-			Logger $logger,
-			ManagerInterface $messageManager,
-			Purger $purger,
-			StoreManagerInterface $storeManager
-		) {
-			// Save the injected class instances
-			$this->_data = $data;
-			$this->_logger = $logger;
-			$this->_messageManager = $messageManager;
-			$this->_purger = $purger;
-			$this->_storeManager = $storeManager;
-		}
+	class Product extends AutoPurge {
 
 		/**
 		 * This method is required because this class implements the ObserverInterface class.  This
@@ -72,45 +27,12 @@
 		public function execute ( Observer $observer ) {
 			// Check to see if event is enabled
 			if ( $this->_data->isEnabled () && $this->_data->shouldPurgeAfterProductSave () ) {
-				// Get the base url for the current store
-				$baseUrl = $this->_storeManager->getStore ()->getBaseUrl ();
-				// Get the product id and retrieve all rewrites recursively and acyclicly.
+				// Get id and purge all urls related to route
 				$pid = $observer->getProduct ()->getId ();
-				$rewrites = $this->_purger->getUrlRewrites ("catalog/product/view/id/$pid");
-				// Define purge command for each found URL
-				$actions = array_fill_keys ( $rewrites, "purgeUrl" );
-				$actions ["catalog/product/view/id/$pid/"] = "purgeStore"; // note the trailing slash
-				// Loop through each rewrite
-				foreach ( $actions as $rewrite => $command ) {
-					// Validate the url
-					$url = $this->_purger->validateUrl ( $baseUrl . $rewrite );
-					// Loop though responses, after purging store (store so we can use 'starts with' for url)
-					foreach ( $this->_purger->{ $command } ( $url ) as $response ) {
-						// Log what we are trying to do
-						$message = [
-							"status" => $response->status,
-							"action" => "auto_purge:product_save",
-							"target" => $response->target,
-							"server" => $response->server
-						];
-						$this->_logger->blame ( $this->_data->getLoggedInUserInfo (), $message );
-						// Check to see if response was successful
-						if ( $response->status == 200 ) {
-							// Add success response message
-							$targetHtml = "<font color='#79A22E' ><b>$response->target</b></font>";
-							$serverHtml = "<font color='#79A22E' ><b>$response->server</b></font>";
-							$message = "Successfully purged varnish cache for $targetHtml on $serverHtml";
-							$this->_messageManager->addSuccess ( $message );
-						}
-						else {
-							// Otherwise add an error message
-							$targetHtml = "<font color='#E22626' ><b>$response->target</b></font>";
-							$serverHtml = "<font color='#E22626' ><b>$response->server</b></font>";
-							$statusHtml = "<font color='#E22626' ><b>$response->status</b></font>";
-							$message = "Error Purging varnish cache for $targetHtml on $serverHtml with response code $statusHtml";
-							$this->_messageManager->addError ( $message );
-						}
-					}
+				$this->_purgeUsingRoute ("catalog/product/view/id/$pid");
+				// Go through all categories associated with product and purge their urls
+				foreach ( $observer->getProduct ()->getCategoryIds () as $cid ) {
+					$this->_purgeUsingRoute ("catalog/category/view/id/$cid");
 				}
 			}
 		}
