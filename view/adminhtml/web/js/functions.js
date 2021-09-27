a2d157d4b4938bea546fbf0294276f18cddeecda
Rafael Grigorian <me@raffi.io>
2021-09-27 14:09:28 -0500

    Related To #51 - 'Better UI/UX for configuration page'

--- view/adminhtml/web/js/functions.js
+++ view/adminhtml/web/js/functions.js
@@ -2,38 +2,6 @@
 
 require (["jquery"], function ( $ ) {
 
-	if ( $(".button-container.steps").length > 0 ) {
-
-		const container = $(".varnish .button-container")
-		const previous = $(container).find (".previous")
-		const next = $(container).find (".next")
-		const message = $(container).find (".title")
-		const count = $(".varnish .card[data-section]").length
-
-		function loadStage ( index ) {
-			const target = $(".varnish .card[data-section]").eq ( index ).data ("section")
-			container.data ( "stage", index )
-			$(message).text (`Step ${index + 1} of ${count}: ${target}`)
-			$(".cards .card").css ( "display", "none" )
-			$(".cards .card").eq ( index ).css ( "display", "block" )
-			$(previous) [ index == 0 ? "removeClass" : "addClass" ] ("active")
-			$(next) [ index >= count - 1 ? "removeClass" : "addClass" ] ("active")
-		}
-
-		$(document).on ( "click", ".previous.active", () => {
-			const stage = container.data ("stage")
-			loadStage ( Math.max ( 0, stage - 1 ) )
-		})
-
-		$(document).on ( "click", ".next.active", () => {
-			const stage = container.data ("stage")
-			loadStage ( Math.min ( count - 1, stage + 1 ) )
-		})
-
-		loadStage ( container.data ("stage") )
-
-	}
-
 	$(document).on ( "click", ".varnish .tray-labels [data-tray]", function () {
 		const tray = $(this).data ("tray")
 		const open = $(this).data ("open")
